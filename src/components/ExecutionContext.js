a678b6404c4fdff9341f1e0a6352893df1265038
DivyanshuRajwar <divyanshurajwar@gmail.com>
2025-03-20 19:50:03 +0530

    Ensure dependencies are installed

--- src/components/ExecutionContext.js
+++ src/components/ExecutionContext.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useRef } from "react";
 import "./Custom.css";
 import FunctionExecutionContext from "./FunctionExecutionContext";
-import { data, pre } from "framer-motion/m";
+import { motion } from "framer-motion";
 import { FaArrowsRotate } from "react-icons/fa6";
 
 function ExecutionContext({
